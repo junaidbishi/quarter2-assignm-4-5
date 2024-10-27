23cb7b91b107d072017538407ee72365a1efd37f
muhammad junaid <junaidbishi02@gmail.com>
2024-10-26 18:23:40 -0700

    add in fotter path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,17 @@
 import Header from "./commonents/header";
 import Header2 from "./commonents/header2";
 import Blogg from "./Blogitem/page";
-import Footer from "./commonents/footer";
+import Footer from "./commonents/Footer";
 
 export default function home() {
   return (
     <div className="bg-gradient-to-r from-black to-grey-200 min-h-screen text-white">
        
         <Header/>
-    
         <Blogg />
-        <div className="otter">
+        <div className="fotter">
           <Footer/>
-        </div>
+          </div>
         
          {/* <Header2/> */}
     </div>
