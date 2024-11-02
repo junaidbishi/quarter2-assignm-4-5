8bfdc82fb0f7a779bf9a4e687c04c9d04288590f
muhammad junaid <junaidbishi02@gmail.com>
2024-11-02 15:08:06 -0700

    use home commponent

--- src/app/Blogitem/page.tsx
+++ src/app/Blogitem/page.tsx
@@ -1,85 +1,85 @@
-import Image from "next/image";
-import freshone from "@/app/Public/freshone.jpg"
-import frehtwo from "@/app/Public/freshtwo.jpg"
-import freshthree from "@/app/Public/freshthree.jpg"
-import freshfour from "@/app/Public/freshfour.jpg"
-import freshfive from "@/app/Public/freshfive.jpg"
-import freshsix from "@/app/Public/freshsix.jpg"
-// import Link from "next/link";
+// import Image from "next/image";
+// import freshone from "@/app/Public/freshone.jpg"
+// import frehtwo from "@/app/Public/freshtwo.jpg"
+// import freshthree from "@/app/Public/freshthree.jpg"
+// import freshfour from "@/app/Public/freshfour.jpg"
+// import freshfive from "@/app/Public/freshfive.jpg"
+// import freshsix from "@/app/Public/freshsix.jpg"
+// // import Link from "next/link";
 
 
-export default function Blogg() {
-  return (
-    <div className="parentContainer">
-      <div className="childContainer tilt-in-right-2" >
-        <div className=" imagecontainerone">
-         <Image src={freshone} alt="bghjk" />
-        </div>
-        <div className="merge">
-        <h1 className="title">this is first block</h1>
-        <p className="description">this is my blog site</p>
-        <button className="readMore">Read More</button>
-        </div>
-      </div>
+// export default function Blogg() {
+//   return (
+//     <div className="parentContainer">
+//       <div className="childContainer tilt-in-right-2" >
+//         <div className=" imagecontainerone">
+//          <Image src={freshone} alt="bghjk" />
+//         </div>
+//         <div className="merge">
+//         <h1 className="title">this is first block</h1>
+//         <p className="description">this is my blog site</p>
+//         <button className="readMore">Read More</button>
+//         </div>
+//       </div>
 
-      <div className="childContainer  tilt-in-right-2">
-        <div className=" imagecontainerone">
-         <Image src={frehtwo} alt="bghjk" width={300} height={100}/>
-        </div>
-        <div className="merge">
-        <h1 className="title">this is first block</h1>
-        <p className="description">this is my blog site</p>
-        <button className="readMore">Read More</button>
-        </div>
-      </div>
+//       <div className="childContainer  tilt-in-right-2">
+//         <div className=" imagecontainerone">
+//          <Image src={frehtwo} alt="bghjk" width={300} height={100}/>
+//         </div>
+//         <div className="merge">
+//         <h1 className="title">this is first block</h1>
+//         <p className="description">this is my blog site</p>
+//         <button className="readMore">Read More</button>
+//         </div>
+//       </div>
 
-      <div className="childContainer  tilt-in-right-2">
-        <div className=" imagecontainerone">
-         <Image src={freshthree} alt="bghjk" width={300} height={100}/>
-        </div>
-        <div className="merge">
-        <h1 className="title">this is first block</h1>
-        <p className="description">this is my blog site</p>
-        <button className="readMore">Read More</button>
-        </div>
-      </div>
+//       <div className="childContainer  tilt-in-right-2">
+//         <div className=" imagecontainerone">
+//          <Image src={freshthree} alt="bghjk" width={300} height={100}/>
+//         </div>
+//         <div className="merge">
+//         <h1 className="title">this is first block</h1>
+//         <p className="description">this is my blog site</p>
+//         <button className="readMore">Read More</button>
+//         </div>
+//       </div>
 
-      <div className="childContainer  tilt-in-right-2">
-        <div className=" imagecontainerone">
-         <Image src={freshfour} alt="bghjk" width={300} height={100}/>
-        </div>
-        <div className="merge">
-        <h1 className="title">this is first block</h1>
-        <p className="description">this is my blog site</p>
-        <button className="readMore">Read More</button>
-        </div>
-      </div>
+//       <div className="childContainer  tilt-in-right-2">
+//         <div className=" imagecontainerone">
+//          <Image src={freshfour} alt="bghjk" width={300} height={100}/>
+//         </div>
+//         <div className="merge">
+//         <h1 className="title">this is first block</h1>
+//         <p className="description">this is my blog site</p>
+//         <button className="readMore">Read More</button>
+//         </div>
+//       </div>
 
-      <div className="childContainer  tilt-in-right-2">
-        <div className=" imagecontainerone">
-         <Image src={freshfive} alt="bghjk" width={300} height={100}/>
-        </div>
-        <div className="merge">
-        <h1 className="title">this is first block</h1>
-        <p className="description">this is my blog site</p>
-        <button className="readMore">Read More</button>
-        </div>
-      </div>
+//       <div className="childContainer  tilt-in-right-2">
+//         <div className=" imagecontainerone">
+//          <Image src={freshfive} alt="bghjk" width={300} height={100}/>
+//         </div>
+//         <div className="merge">
+//         <h1 className="title">this is first block</h1>
+//         <p className="description">this is my blog site</p>
+//         <button className="readMore">Read More</button>
+//         </div>
+//       </div>
 
-      <div className="childContainer  tilt-in-right-2">
-        <div className=" imagecontainerone">
-         <Image src={freshsix} alt="bghjk" width={300} height={100}/>
-        </div>
-        <div className="merge">
-        <h1 className="title">this is first block</h1>
-        <p className="description">this is my blog site</p>
-        <button className="readMore">Read More</button>
-        </div>
-      </div>
-
-
+//       <div className="childContainer  tilt-in-right-2">
+//         <div className=" imagecontainerone">
+//          <Image src={freshsix} alt="bghjk" width={300} height={100}/>
+//         </div>
+//         <div className="merge">
+//         <h1 className="title">this is first block</h1>
+//         <p className="description">this is my blog site</p>
+//         <button className="readMore">Read More</button>
+//         </div>
+//       </div>
 
 
-    </div>
-  )
-}
+
+
+//     </div>
+//   )
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 
-import Header from "./commonents/header";
-import Blogg from "./Blogitem/page";
+// import Header from "./commonents/header";
+// import Blogg from "./Blogitem/page";
 import Footer from "./commonents/Footer";
+import Home from "./Home/page";
 
-export default function Home() {
+
+export default function app() {
   return (
     <div className="bg-gradient-to-r from-black to-grey-200 min-h-screen text-white">
-        <Header/>
-        <Blogg />
-     
+        {/* <Header/> */}
+        {/* <Blogg /> */}
+     <Home/>
           <Footer/>
        
         
