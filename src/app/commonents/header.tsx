0ab0e4306cdcb2727f7a0cc659cb4c0dd683af72
muhammad junaid <junaidbishi02@gmail.com>
2024-11-02 15:49:55 -0700

    remove home contact and about from header

--- src/app/commonents/header.tsx
+++ src/app/commonents/header.tsx
@@ -1,21 +1,18 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import Home from "../Home/page";
-import Aboutus from "../Aboutus/page";
-import contactus from "../Contact-us/page";
 import freshfoodsix from "@/app/Public/freshfoodsix.png"
 
 export default function Header(){
     return(
-    <div className="header">
+    <div className="header" >
     <ul className="headerbuttons" > 
     <div className=" logo">
         <button > <Image src={freshfoodsix} alt={"logo"}  /></button>
         </div>
        
-   <Link href={"/"} ><li>   <button className="hover">Home</button></li></Link>
-<Link href={"/Aboutus"}><li><button className="hover"> About-us</button></li></Link>
+   <Link href={"/"} ><li><button className="hover">Home</button></li></Link>
+<Link href={"/Aboutus"}><li><button className="hover">About-us</button></li></Link>
     <Link href={"/Contact-us"}><li><button className="hover">Contact-us</button></li></Link>
   
     </ul>
